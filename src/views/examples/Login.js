6062919f825cb9406435ec4eb12f4d8c10e5cf59
LLZ248 <61862608+LLZ248@users.noreply.github.com>
2021-09-20 20:05:48 +0800

    Remove unnecessary elements
    Edit the template so it matches our project

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -11,71 +11,30 @@ import {
   InputGroupText,
   InputGroup,
   Row,
-  Col,
+  Col
 } from "reactstrap";
-
+import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <>
       <Col lg="5" md="7">
-        <Card className="bg-secondary shadow border-0">
-          <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-3">
-              <small>Sign in with</small>
-            </div>
-            <div className="btn-wrapper text-center">
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/github.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/google.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
-            </div>
-          </CardHeader>
+        <Card className="bg-secondary shadow border-0 mt-5">
           <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>Or sign in with credentials</small>
+            <div className="text-center text-bold mb-4">
+              <h1>Sign In As Administrator</h1>
             </div>
             <Form role="form">
               <FormGroup className="mb-3">
                 <InputGroup className="input-group-alternative">
                   <InputGroupAddon addonType="prepend">
                     <InputGroupText>
-                      <i className="ni ni-email-83" />
+                      <i className="ni ni-circle-08" />
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Email"
-                    type="email"
-                    autoComplete="new-email"
+                    placeholder="username"
+                    type="text"
+                    autoComplete
                   />
                 </InputGroup>
               </FormGroup>
@@ -114,26 +73,16 @@ const Login = () => {
             </Form>
           </CardBody>
         </Card>
-        <Row className="mt-3">
-          <Col xs="6">
-            <a
+        <div className="text-center mt-2">
+            <Link
               className="text-light"
-              href="#pablo"
-              onClick={(e) => e.preventDefault()}
+              to="/auth/register"
+              tag={Link}
             >
-              <small>Forgot password?</small>
-            </a>
-          </Col>
-          <Col className="text-right" xs="6">
-            <a
-              className="text-light"
-              href="#pablo"
-              onClick={(e) => e.preventDefault()}
-            >
-              <small>Create new account</small>
-            </a>
-          </Col>
-        </Row>
+              <small>New Administrator? Sign Up Now</small>
+            </Link>
+          
+        </div>
       </Col>
     </>
   );
--- src/views/examples/Register.js
+++ src/views/examples/Register.js
@@ -1,8 +1,9 @@
+import React, { Component } from 'react';
+import CreatableSelect from 'react-select/creatable';
 // reactstrap components
 import {
   Button,
   Card,
-  CardHeader,
   CardBody,
   FormGroup,
   Form,
@@ -14,134 +15,241 @@ import {
   Col,
 } from "reactstrap";
 
-const Register = () => {
-  return (
-    <>
-      <Col lg="6" md="8">
-        <Card className="bg-secondary shadow border-0">
-          <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-4">
-              <small>Sign up with</small>
-            </div>
-            <div className="text-center">
-              <Button
-                className="btn-neutral btn-icon mr-4"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/github.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/google.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
-            </div>
-          </CardHeader>
-          <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>Or sign up with credentials</small>
-            </div>
-            <Form role="form">
-              <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-hat-3" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input placeholder="Name" type="text" />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-email-83" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Email"
-                    type="email"
-                    autoComplete="new-email"
-                  />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-lock-circle-open" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Password"
-                    type="password"
-                    autoComplete="new-password"
-                  />
-                </InputGroup>
-              </FormGroup>
-              <div className="text-muted font-italic">
-                <small>
-                  password strength:{" "}
-                  <span className="text-success font-weight-700">strong</span>
-                </small>
+type State = {
+  options: [{ [string]: string }],
+  value: string | void,
+};
+
+const createOption = (label: string) => ({
+  label,
+  value: label.toLowerCase().replace(/\W/g, ''),
+});
+
+const defaultOptions = [
+  createOption('One'),
+  createOption('Two'),
+  createOption('Three'),
+];
+
+
+class Register extends Component<*, State> {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleHealthcareChange = this.handleHealthcareChange.bind(this);
+    this.roleChange = this.roleChange.bind(this);
+    this.handleHealthcareCreate = this.handleHealthcareCreate.bind(this);
+  }
+
+  state = {
+    isLoading: false,
+    options: defaultOptions,
+    value: undefined,
+  }
+
+
+  roleChange(event) {
+    this.setState({
+      role: event.target.value
+    });
+  }
+
+  handleHealthcareChange = (newValue: any, actionMeta: any) => {
+    this.setState({ value: newValue });
+  };
+
+  handleHealthcareCreate = (inputValue: any) => {
+    this.setState({ isLoading: true });
+    //TODO: Add backend action here
+    console.group('Option created');
+    console.log('Wait a moment...');
+    setTimeout(() => {
+      const { options } = this.state;
+      const newOption = createOption(inputValue);
+      console.log(newOption);
+      console.groupEnd();
+      this.setState({
+        isLoading: false,
+        options: [...options, newOption],
+        value: newOption,
+      });
+    }, 1000);
+  };
+
+  render(){
+    const { isLoading, options, value } = this.state;
+    return (
+      <>
+        <Col lg="6" md="8">
+          <Card className="bg-secondary shadow border-0">
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center mb-4">
+                <h1>Sign Up</h1>
               </div>
-              <Row className="my-4">
-                <Col xs="12">
-                  <div className="custom-control custom-control-alternative custom-checkbox">
+              <Form role="form">
+                <FormGroup>
+                  <Row className="my-4">
+                  <div className="custom-control custom-radio mx-auto">
                     <input
                       className="custom-control-input"
-                      id="customCheckRegister"
-                      type="checkbox"
+                      id="adminstratorRadio"
+                      name="role-radio"
+                      type="radio"
+                      value="administrator"
+                      checked={this.state.role === "administrator"}
+                      onChange={this.roleChange}
                     />
-                    <label
-                      className="custom-control-label"
-                      htmlFor="customCheckRegister"
-                    >
-                      <span className="text-muted">
-                        I agree with the{" "}
-                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                          Privacy Policy
-                        </a>
-                      </span>
+                    <label className="custom-control-label" htmlFor="adminstratorRadio">
+                      Adminstrator
                     </label>
                   </div>
-                </Col>
-              </Row>
-              <div className="text-center">
-                <Button className="mt-4" color="primary" type="button">
-                  Create account
-                </Button>
-              </div>
-            </Form>
-          </CardBody>
-        </Card>
-      </Col>
-    </>
-  );
+                  <div className="custom-control custom-radio mx-auto">
+                    <input
+                      className="custom-control-input"
+                      defaultChecked
+                      id="patientRadio"
+                      name="role-radio"
+                      type="radio"
+                      value="patient"
+                      checked={this.state.role === "patient"}
+                      onChange={this.roleChange}
+                    />
+                    <label className="custom-control-label" htmlFor="patientRadio">
+                      Patient
+                    </label>
+                  </div>
+                  </Row>
+                </FormGroup>
+                {this.state.role === "administrator" &&
+                  <div className="admin-only">
+                    <FormGroup>
+                      <label>Healthcare Centre</label>
+                      <CreatableSelect
+                        isClearable
+                        isDisabled={isLoading}
+                        isLoading={isLoading}
+                        onChange={this.handleHealthcareChange}
+                        onCreateOption={this.handleHealthcareCreate}
+                        options={options}
+                        value={value}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Input disabled placeholder="Healthcare Address" type="text" />
+                    </FormGroup>
+                  </div>
+                }
+                <label>Your Personal Information</label>
+
+                <FormGroup>
+                  <InputGroup className="input-group-alternative mb-3">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-single-02" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="form-control-username"
+                      placeholder="Username"
+                      type="text"
+                    />
+                  </InputGroup>
+                </FormGroup>
+
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="form-control-password"
+                      placeholder="Pasword"
+                      type="password"
+                    />
+                  </InputGroup>
+                </FormGroup>
+
+                <FormGroup>
+                  <InputGroup className="input-group-alternative mb-3">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-circle-08" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="form-control-fullname"
+                      placeholder="Full Name"
+                      type="text"
+                    />
+                  </InputGroup>
+                </FormGroup>
+                  
+                <FormGroup>
+                  <InputGroup className="input-group-alternative mb-3">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="form-control-email"
+                      placeholder="Email"
+                      type="email"
+                    />
+                  </InputGroup>
+                </FormGroup>
+                
+                {
+                  this.state.role === "administrator"?
+                  <div>
+                    <FormGroup className="admin-only">
+                      <InputGroup className="input-group-alternative mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="ni ni-badge" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          id="form-control-staffid"
+                          placeholder="Staff ID"
+                          type="text"
+                        />
+                      </InputGroup>
+                  </FormGroup>
+                  </div> :
+                    <div>
+                      <FormGroup className="admin-only">
+                        <InputGroup className="input-group-alternative mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="ni ni-badge" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            id="form-control-icpassport"
+                            placeholder="IC/Passport No"
+                            type="text"
+                          />
+                        </InputGroup>
+                      </FormGroup>
+                  </div>
+                }
+                
+                <div className="text-center">
+                  <Button className="mt-4" color="primary" type="button">
+                    Sign Up
+                  </Button>
+                </div>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </>
+    );
+  }
+  
 };
 
 export default Register;

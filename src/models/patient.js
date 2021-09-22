83f3b23aaa45989750f656c8010bf91198ac6a34
LLZ248 <61862608+LLZ248@users.noreply.github.com>
2021-09-22 16:23:21 +0800

    Create patient.js

---
+++ src/models/patient.js
@@ -0,0 +1,27 @@
+const crypto = require('crypto');
+
+class Patient{
+    constructor(username,password,email,fullName,ICPassport){
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+        this.ICPassport = ICPassport;
+        this.setPassword;
+    }
+    constructor(username, password){
+        this.username = username;
+        this.password = password;
+        
+    }
+
+    setPassword(password){
+        var newPassword;
+        var hash = crypto.createHash('sha256');
+        data = hash.update(password);
+        newPassword= data.digest('hex');
+        this.password = newPassword;
+    }
+
+    
+    
+}
